97cffe98338e806774f6cfb490f756f0452b88c6
Andy Zhang <andzhng@gmail.com>
2015-11-07 23:28:54 -0800

    Use medium-editor instead of quill

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -30,20 +30,47 @@ var Content = React.createClass({
 });
 
 var Editor = React.createClass({
+
+    _PLACEHOLDER: {
+        text: 'How was your day?'
+    },
+
+    getInitialState: function() {
+        return {
+            editor: null,
+        };
+    },
+
     componentDidMount: function() {
-        var editor = new Quill('.jl-editor');
-        editor.addModule('placeholder', {
-            text: 'How was your day?',
-            style: { color: '#999999' }
+        // var editor = new Quill('.jl-editor');
+        // this._setupModule(editor);
+        var editor = new MediumEditor('.jl-editor', {
+            toolbar: false,
+            // Disables having 2 empty lines
+            disableDoubleReturn: false,
+            // Enable image dragging
+            imageDragging: true,
+            // Disables any keyboard shortcuts
+            keyboardCommands: false,
+            placeholder: this._PLACEHOLDER,
+            // Disables the toolbar
         });
+        this.setState({
+            editor: editor
+        }, this._setupModule);
     },
 
     render: function() {
         return (
-            <div className='jl-editor'>
+            <div className='jl-editor' data-disable-toolbar="true">
             </div>
         );
-    }
+    },
+
+    _onEditorTextChange: function(delta, source) {
+        this.state.editor.getBounds
+    },
+
 });
 
 ReactDOM.render(
