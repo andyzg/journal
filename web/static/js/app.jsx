f11cd6028d960ddb0a828ffbf9c6fcd9abea1354
Andy Zhang <andzhng@gmail.com>
2015-11-16 23:27:55 -0800

    Styled editor to start at the bottom

--- web/static/js/app.jsx
+++ web/static/js/app.jsx
@@ -2,43 +2,57 @@
 
 import _ from 'underscore';
 
-var JournalContainer = React.createClass({
-    render: function() {
+const ENTER_KEY = 13;
+
+let JournalContainer = React.createClass({
+    render() {
         return (
             <div className='jl-container'>
                 <Header />
-                <Content />
+                <Content onRender={this.props.onRender} />
+                <Footer />
             </div>
         );
     }
 });
 
-var Header = React.createClass({
-render: function() {
-    return (
-        <div className='jl-header'>
-            <img className='jl-logo' src='static/img/logo.png' />
-        </div>
+let Header = React.createClass({
+    render() {
+        return (
+            <div className='jl-header'>
+                <img className='jl-logo' src='static/img/logo.png' />
+            </div>
         );
     }
 });
 
-var Content = React.createClass({
-    getInitialState: function() {
+let Footer = React.createClass({
+    render() {
+        return (
+            <div className='jl-footer'>
+            </div>
+        );
+    }
+});
+
+let Content = React.createClass({
+    getInitialState() {
         return {
             entries: []
         };
     },
 
-    componentDidMount: function() {
-        $.get('/api/entry', function(data) {
+    componentDidMount() {
+        $.get('/api/entry', data => {
             // TODO: Create object models for entries
-            this.setState({entries: data.entries});
-        }.bind(this));
+            this.setState({entries: data.entries}, () => {
+                this.props.onRender();
+            });
+        });
     },
 
-    render: function() {
-        var entries = [];
+    render() {
+        let entries = [];
         for (var i = 0; i < this.state.entries.length; i++) {
             entries.push(
                 <Editor text={this.state.entries[i].content} 
@@ -58,27 +72,40 @@ var Content = React.createClass({
     }
 });
 
-var Editor = React.createClass({
+let Editor = React.createClass({
 
     _PLACEHOLDER: 'How was your day?',
 
-    getInitialState: function() {
+    _EDITABLE_SELECTOR: '.jl-editor[contenteditable=true]',
+
+    getInitialState() {
         return {
+            // TODO: I shouldn't have to be making such a complicated check.
+            contentEditable: !(this.props.isLocked.toString().toLowerCase() === 'true'),
             editor: null,
         };
     },
 
-    render: function() {
-        var contentEditable = true;
-        if (this.props.isLocked) {
-            contentEditable = false;
+    componentDidMount() {
+        if (!this.state.contentEditable) {
+            return;
         }
+        $(this._EDITABLE_SELECTOR).on('keydown', e => {
+            if (e.which === ENTER_KEY) {
+                let content = $(this._EDITABLE_SELECTOR).text();
+                this._postEntry(e, content);
+            }
+        });
+    },
 
-        var classesList = [];
+    render() {
+        const contentEditable = this.state.contentEditable;
+        let classesList = [];
+
         if (!contentEditable) {
             classesList.push('jl-locked');
         }
-        var classes = Array.prototype.join.call(classesList, ' ');
+        let classes = Array.prototype.join.call(classesList, ' ');
         return (
             <div className={'jl-editor ' + classes}
                  contentEditable={contentEditable}
@@ -88,9 +115,14 @@ var Editor = React.createClass({
         );
     },
 
-    _onEditorTextChange: function(delta, source) {
-        this.state.editor.getBounds
-    },
+    _postEntry(e, content) {
+        e.preventDefault();
+        if (!content) {
+            return;
+        }
+        alert(content);
+        this.setState({ contentEditable: false });
+    }
 
 });
 
--- web/static/js/index.jsx
+++ web/static/js/index.jsx
@@ -3,7 +3,11 @@
 import React from 'react';
 import JournalContainer from './app';
 
+let onRender = function() {
+    window.scrollTo(0, document.body.clientHeight); 
+};
+
 ReactDOM.render(
-    <JournalContainer />,
+    <JournalContainer onRender={onRender}/>,
     document.getElementById('content')
 );
